5f7a1d5c7ad713c0889245bc6652a3cca1a0c3da
BhaskarDey772 <bhaskardey772@gmail.com>
2022-07-10 21:29:27 +0530

    project completed

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import { Grid} from '@material-ui/core';
+import { useStyles } from '../style/AppStyle';
+
+import { useNavigate } from 'react-router-dom';
+import HomeIcon from '@material-ui/icons/Home';
+
+const Header = () => {
+  const classes = useStyles();
+  const navigate  = useNavigate()
+  return (
+        <Grid item xs={12} style={{marginBottom:"1rem" }}>
+     
+          <Toolbar className={classes.paper} style={{backgroundColor:"#3f50b4"}}>
+            <IconButton
+                edge="start"
+                className={classes.menuButton}
+                color="inherit"
+                aria-label="open drawer"
+                onClick={()=>navigate("/" , { replace: true })}
+            >
+                <HomeIcon style={{color:"white"}}  />
+            </IconButton>
+            <Typography style={{color:"white"}}>
+                Assignment One
+            </Typography>
+          
+          
+          </Toolbar>
+        </Grid>
+  );
+}
+
+export default Header
